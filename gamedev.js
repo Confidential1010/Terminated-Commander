6585d7a749cc174632eadd045328ce7a6e860839
Confidential1010 <210403@hkis.edu.hk>
2016-05-25 09:32:30 +0800

    added typed text

--- gamedev.js
+++ gamedev.js
@@ -8,8 +8,8 @@
 <script>
   $(function(){
       $("#typing_text").typed({
-        strings: ["Captain! Captain!!!",<br  /> "We have arrived on the planet Zandar and we have detected hostile life forms on the surface. Do you wish to proceed to the surface or go back into orbit? "],
-        typeSpeed: 10;
+        strings: ["Captain! Captain!!!",<br  /> "We have arrived on the planet Zandar and we have detected hostile life forms on the surface. Do you wish to proceed to the surface or go back into orbit?"],
+        typeSpeed: 0;
       });
   });
 </script>
