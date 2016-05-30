07518085fad252f364b1800cd08864ba4a32efd1
Confidential1010 <210403@hkis.edu.hk>
2016-05-30 21:08:22 +0800

    changed font color

--- gamedev.js
+++ gamedev.js
@@ -1,20 +1,7 @@
-<head>
-<link rel="stylesheet" type="text/css" href="style.css">
-<head>
-<script src="jquery-1.11.3.js"></script>
-<script src="gamedev.js"></script>
-</head>
-<script src="typed.js"></script>
-<script>
+
   $(function(){
       $("#typing_text").typed({
         strings: ["Captain! Captain!!!",<br  /> "We have arrived on the planet Zandar and we have detected hostile life forms on the surface. Do you wish to proceed to the surface or go back into orbit?"],
         typeSpeed: 0;
       });
-  });
-</script>
-...
-
-<div class="element"></div>
-
 //Your jquery goes here
