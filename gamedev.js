635bb541f61f4843010337a7a39d0f25369ec361
Confidential1010 <210403@hkis.edu.hk>
2016-05-31 09:09:47 +0800

    finished text

--- gamedev.js
+++ gamedev.js
@@ -1,7 +1,3 @@
 
-  $(function(){
-      $("#typing_text").typed({
-        strings: ["Captain! Captain!!!",<br  /> "We have arrived on the planet Zandar and we have detected hostile life forms on the surface. Do you wish to proceed to the surface or go back into orbit?"],
-        typeSpeed: 0;
-      });
+
 //Your jquery goes here
